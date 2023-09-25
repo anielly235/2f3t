e159c1675dfe05ad6309fd574a7878ba20b404ea
anielly235 <110236221+anielly235@users.noreply.github.com>
2023-09-25 07:56:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 mensagem = (msg) => alert (msg);
-soma = (a,b) => a + b;
 
-function mensagem(msg) {
-    alert("ola" + msg);
+soma = (a,b) => a+b;
+sub = (a,b) => a-b;
+mult = (a,b) => a*b;
+div = (a,b) => a/b;
+raiz = x => Math.sqrt(x);
 
-}
-/*
-// f(x) = 2x + 2
-// f(2) = 2.2 + 2
-// f(2) = 6
-*/
+//mensagem(soma (10, soma(8+2)));
+//mensagem (soma (2, div(2,2)));
+//mensagem (soma (div(80,2) ,mult(5,3)));
+//mensagem (soma (sub(8,5), sub(3,70)));
